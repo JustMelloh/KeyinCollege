21ca63c779b783fbc444d224114c2ba7161c7ae6
JustMelloh <austinaustin2009@live.ca>
2023-12-05 21:09:39 -0330

    Added Notes to Q3.

--- S2 Classes/JavaScript/Practice Assignments/December4(Testing)/my-app/src/Components/Holidays/happyholidays.test.jsx
+++ S2 Classes/JavaScript/Practice Assignments/December4(Testing)/my-app/src/Components/Holidays/happyholidays.test.jsx
@@ -1,7 +1,9 @@
 // Question 3 Testing
 
 import React from 'react';
-import {render, screen, waitFor} from '@testing-library/react';
+import {render, screen, waitFor, act} from '@testing-library/react'; 
+//Added Act to signal to React that it is a status update. This helps ensure the test acts 
+//more like it would in a real environ.
 import HappyHolidays from './HappyHolidays';
 
 jest.useFakeTimers();
@@ -18,9 +20,12 @@ test('Dont initialize "Happy Holidays Hurrah!"', () => {
     expect(happyHolidaysHeading).not.toBeInTheDocument();
 });
 
-test('initialize "Happy Holidays Hurrah!"', async () => {
+test('initialize "Happy Holidays Hurrah!"', async () => { // Must make it async due to the await keyword which waits for the time to elapse before passing.
     render(<HappyHolidays />);
-    jest.advanceTimersByTime(3000); 
+
+    act(() => {
+        jest.advanceTimersByTime(3000); // 3000 = 3 Seconds.
+      });
 
     await waitFor(() => {
         const happyHolidaysHeading = screen.getByText(/Happy Holidays Hurrah!/i);
