d3d2efddcc0aa3d08f4a06c3996f400132ff1f8d
JustMelloh <austinaustin2009@live.ca>
2023-12-21 04:53:54 -0330

    Product Page needs to be done.

---
+++ S2 Classes/Sprints/Sprint2/Portfolio/scripts/aboutScript.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var aboutBtn1 = document.getElementById('aboutBtn1');
+    var aboutBox1Text = document.querySelector('.aboutBox1-text');
+
+    var aboutBtn2 = document.getElementById('aboutbtn2');
+    var aboutBox2Text = document.querySelector('.aboutBox2-text');
+
+    aboutBtn1.addEventListener('click', function () {
+        // Toggle the display of aboutBox1 text when the button is clicked
+        aboutBox1Text.style.display = (aboutBox1Text.style.display === 'none' || aboutBox1Text.style.display === '') ? 'block' : 'none';
+    });
+
+    aboutBtn2.addEventListener('click', function () {
+        // Toggle the display of aboutBox2 text when the button is clicked
+        aboutBox2Text.style.display = (aboutBox2Text.style.display === 'none' || aboutBox2Text.style.display === '') ? 'block' : 'none';
+    });
+});
--- S2 Classes/Sprints/Sprint2/Portfolio/scripts/eduScript.js
+++ S2 Classes/Sprints/Sprint2/Portfolio/scripts/eduScript.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var portfolioMoreButton = document.getElementById('portfolioMoreButton');
-    var portfolioMoreText = document.querySelector('.portfolio-moreTxt');
+document.addEventListener('DOMContentLoaded', function () {
+    // Add a click event listener to the button
+    var educationMoreButton = document.getElementById('educationMoreButton');
+    var educationMoreTxt = document.querySelector('.education-moreTxt');
 
-    // Hide the elements initially
-    portfolioMoreText.classList.add('hidden');
-
-    portfolioMoreButton.addEventListener('click', function() {
-        portfolioMoreText.classList.toggle('hidden');
+    educationMoreButton.addEventListener('click', function () {
+        // Toggle the display of education details when the button is clicked
+        if (educationMoreTxt.style.display === 'none' || educationMoreTxt.style.display === '') {
+            educationMoreTxt.style.display = 'block';
+        } else {
+            educationMoreTxt.style.display = 'none';
+        }
     });
 });
