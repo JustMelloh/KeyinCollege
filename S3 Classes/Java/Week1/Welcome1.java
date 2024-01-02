ca9e9dabb5a8e0100aaa4caa21e9013ee2f021d3
JustMelloh <austinaustin2009@live.ca>
2024-01-02 18:44:13 -0330

    Semester 3 Starts!

---
+++ S3 Classes/Java/Week1/Welcome1.java
@@ -0,0 +1,12 @@
+public class Welcome1 {
+
+    // Main method begins execution of java application
+    public static void main( String args[] )
+    {
+    
+    System.out.println("Welcome to Java Programming!");
+
+    } // End Method Main
+
+} // End class Welcome1
+
